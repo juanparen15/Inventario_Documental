a9c1bb927ebeb1aa96abbe1e786ad5d1886ff221
juanparen15 <jprendon9@gmail.com>
2024-01-03 09:10:17 -0500

    Cambio de imagen de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Inventario Documental')
 
 @section('content')
-<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+<div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(homeland/images/hero_bg_2.jpg); height: 940px" data-aos="fade" data-stellar-background-ratio="0.8">
     <div class="container">
       <div class="row align-items-center justify-content-center text-center">
         <div class="col-md-10">
